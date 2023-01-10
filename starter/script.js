d216fc9f5721a7ff10c23a746ef68819ae8a7552
Mochi-coder <116069253+Mochi-coder@users.noreply.github.com>
2023-01-10 11:57:43 +0000

    completed code

--- starter/script.js
+++ starter/script.js
@@ -90,41 +90,111 @@ var upperCasedCharacters = [
 
 // Function to prompt user for password options
 function getPasswordOptions() {
-var length = parseInt (
-  prompt("Please choose a password between 10 and 64 characters")
-);
+  var length = parseInt(
+    prompt('How many characters would you like your password to contain?')
+  );
 
-if (isNaN(length === true)) {
-  alert('Password must be a minimum of 10 and a maximum of 64 characters');
-return;
+  if (isNaN(length) === true) {
+    alert('Please enter a number for your password length');
+    return;
+  }
+
+  if (length < 10) {
+    alert('The password length must be more than 10 characters');
+    return;
+  }
+
+  if (length > 64) {
+    alert('The password length must less than 64 characters');
+    return;
+  }
+
+  var includesSpecialCharacters = confirm(
+    'Click OK for the password to include special characters'
+  );
+
+  var includesNumericCharacters = confirm(
+    'Click OK for the password to include numeric characters'
+  );
+
+  var includesLowerCasedCharacters = confirm(
+    'Click OK for the password to include lowercase characters'
+  );
+
+  var includesUpperCasedCharacters = confirm(
+    'Click OK for the password to include uppercase characters'
+  );
+
+  if (
+    includesSpecialCharacters === false &&
+    includesNumericCharacters === false &&
+    includesLowerCasedCharacters === false &&
+    includesUpperCasedCharacters === false
+  ) {
+    alert('Your password must include at least one character type');
+    return;
+  }
+
+  var passwordOptions = {
+    length: length,
+    includesSpecialCharacters: includesSpecialCharacters,
+    includesNumericCharacters: includesNumericCharacters,
+    includesLowerCasedCharacters: includesLowerCasedCharacters,
+    includesUpperCasedCharacters: includesUpperCasedCharacters
+  };
+
+  return passwordOptions;
 }
-
-if (length < 10) {
-  alert("Password must be a minimum of 10 characters");
-return;
-}
-
-if (length > 64) {
-  alert("Password length must be a maximum of 64 characters");
-return;
-}
-
-var includesCharacterTypes = confirm (
-'Please click OK to confirm at least one character type is used'
-);
-
-}
-
 
 // Function for getting a random element from an array
 function getRandom(arr) {
+  var randIndex = Math.floor(Math.random() * arr.length);
+  var randElement = arr[randIndex];
 
+  return randElement;
 }
 
 // Function to generate password with user input
 function generatePassword() {
-alert("Called this function. Wheeee!");
-return "This is a secret password. Whee."
+  var options = getPasswordOptions();
+
+  var result = [];
+
+  var possibleCharacters = [];
+
+  var guaranteedCharacters = [];
+
+
+  if (options.includesSpecialCharacters) {
+    possibleCharacters = possibleCharacters.concat(specialCharacters);
+    guaranteedCharacters.push(getRandom(specialCharacters));
+  }
+
+  if (options.includesNumericCharacters) {
+    possibleCharacters = possibleCharacters.concat(numericCharacters);
+    guaranteedCharacters.push(getRandom(numericCharacters));
+  }
+
+  if (options.includesLowerCasedCharacters) {
+    possibleCharacters = possibleCharacters.concat(lowerCasedCharacters);
+    guaranteedCharacters.push(getRandom(lowerCasedCharacters));
+  }
+
+  if (options.includesUpperCasedCharacters) {
+    possibleCharacters = possibleCharacters.concat(upperCasedCharacters);
+    guaranteedCharacters.push(getRandom(upperCasedCharacters));
+  }
+
+  for (var i = 0; i < options.length; i++) {
+    var possibleCharacter = getRandom(possibleCharacters);
+    result.push(possibleCharacter);
+  }
+
+  for (var i = 0; i < guaranteedCharacters.length; i++) {
+    result[i] = guaranteedCharacters[i];
+  }
+
+  return result.join('');
 }
 
 // Get references to the #generate element
